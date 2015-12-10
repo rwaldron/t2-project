6b534fc04893a78e1adbda5f86b87599b5f403dd
Rick Waldron <waldron.rick@gmail.com>
2015-12-10 15:32:46 -0500

    Tests for package.json inclusion

--- test/unit/project.prototype.collect.js
+++ test/unit/project.prototype.collect.js
@@ -77,54 +77,26 @@ exports['Project.prototype.collect with node_modules'] = {
     done();
   },
 
-  // returnsPromise: function(test) {
-  //   test.expect(5);
-  //   var p = this.project.collect();
-
-  //   test.equal(p instanceof Promise, true);
-
-  //   p.then((entries) => {
-  //     ['a', 'b', 'c', 'index'].forEach((name) => {
-  //       test.ok(entries.find(entry => entry.id.endsWith(name + '.js')));
-  //     });
-  //     test.done();
-  //   });
-  // },
-
   providesPackageJson: function(test) {
-    // test.expect(5);
+    test.expect(6);
+
     this.project.collect(function(error, entries) {
 
       test.equal(error, null);
 
-      console.log(entries.length);
-      ['', 'index'].forEach((name) => {
-        test.ok(entries.find(entry => entry.id.endsWith(name + '.js')));
+      var expected = [
+        '/package.json',
+        '/node_modules/math-euler/package.json',
+        '/node_modules/math-euler/euler.js',
+        '/index.js',
+      ];
+
+      test.equal(entries.length, 4);
+
+      expected.forEach((name) => {
+        test.ok(entries.find(entry => entry.id.endsWith(name)), name);
       });
       test.done();
     });
   },
-
-  // removesExcludedFiles: function(test) {
-  //   test.expect(6);
-
-  //   this.project.exclude(['b.js', 'c.js']);
-
-  //   this.project.collect(function(error, entries) {
-
-  //     test.equal(error, null);
-  //     test.equal(entries.length, 2);
-
-  //     ['a', 'index'].forEach((name) => {
-  //       test.ok(entries.find(entry => entry.id.endsWith(name + '.js')));
-  //     });
-
-  //     // These will not be present
-  //     ['b', 'c'].forEach((name) => {
-  //       test.equal(entries.find(entry => entry.id.endsWith(name + '.js')), undefined);
-  //     });
-
-  //     test.done();
-  //   });
-  // },
 };
