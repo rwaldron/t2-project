e80fb0269d315eb4a8c56ce5d128dfdb2393c422
Rick Waldron <waldron.rick@gmail.com>
2015-12-10 15:58:52 -0500

    Package.json: Clean up testing; remove project directory example

--- test/smoke/index.js
+++ test/smoke/index.js
@@ -1,51 +1,27 @@
 var Project = require('../../');
 
-var a = new Project({
-  entry: './eg/project-uses-socket-io/index.js',
-});
-
-a.collect(function(error, entries) {
-  console.log('DONE: a', entries.length);
-  entries.forEach(entry => {
-    if (entry.file.endsWith('package.json')) {
-      console.log(entry.file);
-    }
-  });
-});
-
-
 var b = new Project({
   entry: './eg/project-simple/index.js',
 });
 
 b.collect(function(error, entries) {
   console.log('DONE: a', entries.length);
-  entries.forEach(entry => {
-    if (entry.file.endsWith('package.json')) {
-      console.log(entry.file);
-      console.log(entry.source);
-    }
-
-  });
-  // console.log(entries);
+  entries.forEach(entry => console.log(entry.file));
 });
 
+var c = new Project({
+  entry: './eg/project-conditional/index.js',
+});
 
+c.exclude(['b.js']);
+c.exclude(['c.*']);
 
+var p = c.collect(function(error, entries) {
+  console.log('CALLBACK: c', entries.length);
+  entries.forEach(entry => console.log(entry.file));
+});
 
-// var c = new Project({
-//   entry: './eg/project-conditional/index.js',
-// });
-
-// c.exclude(['b.js']);
-// c.exclude(['c.*']);
-
-// var p = c.collect(function(error, entries) {
-//   console.log('CALLBACK: c', entries.length);
-//   console.log(entries);
-// });
-
-// p.then(entries => {
-//   console.log('RESOLVED: c', entries.length);
-//   console.log(entries);
-// });
+p.then(entries => {
+  console.log('RESOLVED: c', entries.length);
+  entries.forEach(entry => console.log(entry.file));
+});
