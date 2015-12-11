8bf8026e54eb8ec66c84d2d936b953b95da528d7
Rick Waldron <waldron.rick@gmail.com>
2015-12-11 11:13:57 -0500

    readme: better examples of exclude()

--- test/smoke/index.js
+++ test/smoke/index.js
@@ -13,8 +13,21 @@ var b = new Project({
   entry: './eg/project-conditional/index.js',
 });
 
-b.exclude(['b.js']);
-b.exclude(['c.*']);
+b.exclude(['b.js', 'c.*']);
+
+/*
+Or...
+
+b.exclude('b.js');
+b.exclude('c.*');
+
+Or...
+
+b.exclude({
+  files: ['b.js', 'c.*'],
+  basedir: (optionally specify a basedir)
+});
+*/
 
 var p = b.collect((error, entries) => {
   console.log('CALLBACK: b', entries.length);
