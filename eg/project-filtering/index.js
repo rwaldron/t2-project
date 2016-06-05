c32112a65375135b52fc13b02df0a8ac1315b851
Rick Waldron <waldron.rick@gmail.com>
2016-06-05 12:09:01 -0400

    Use multi-line comments
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- eg/project-filtering/index.js
+++ eg/project-filtering/index.js
@@ -1,8 +1,10 @@
-// Old built-ins may appear in conditional requires
-// which will lead to inclusion in the graph.
-// This examples shows that filtered built-ins,
-// old or new are ignored.
-//
-// This doesn't belong in "project-conditional"
-// because it's specifically testing filter semantics.
+/*
+  Old built-ins may appear in conditional requires
+  which will lead to inclusion in the graph.
+  This examples shows that filtered built-ins,
+  old or new are ignored.
+
+  This doesn't belong in "project-conditional"
+  because it's specifically testing filter semantics.
+*/
 var conditional = false ? require('sys') : require('util');
